a94a58dbc0fdda1b7bd1286804ff10620af9abff
YehyaYounes <yehyayounes15@gmail.com>
2023-12-27 16:30:27 +0200

    innit

---
+++ cute_kids/src/pages/Home.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+
+   <>
+    <header>
+               <h2 className=' bg-red-600 ' >Quantum physics</h2> 
+        </header>
+    <div className=' border-2 m-2 flex'>Home
+      <h1 className=' text-start bg-black text-white'>home</h1>
+    </div>
+   </>
+  )
+}
+
+export default Home
